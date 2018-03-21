c25bb76818d2c33663d89b170af1f3eac30de678
montgo64 <37602722+montgo64@users.noreply.github.com>
2018-03-20 20:53:45 -0400

    Prototype GUI
    First commit to master

---
+++ GlitchArtEditor/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+
+using Microsoft.Win32;
+
+namespace GlitchArtEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                string test1 = op.FileName;
+                Uri test2 = new Uri(op.FileName);
+
+                imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void ExportImage(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Title = "Select a picture";
+            save.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+
+            if (save.ShowDialog() == true)
+            {
+                SaveUsingEncoder(imgPhoto, save.FileName);
+            }
+        }
+
+        void SaveUsingEncoder(FrameworkElement visual, string fileName)
+        {
+            var encoder = new JpegBitmapEncoder();
+            RenderTargetBitmap bitmap = new RenderTargetBitmap(
+                (int)visual.ActualWidth,
+                (int)visual.ActualHeight,
+                96,
+                96,
+                PixelFormats.Pbgra32);
+            bitmap.Render(visual);
+            BitmapFrame frame = BitmapFrame.Create(bitmap);
+            encoder.Frames.Add(frame);
+
+            using (var stream = File.Create(fileName))
+            {
+                encoder.Save(stream);
+            }
+        }
+
+        private void swap(object sender, RoutedEventArgs e)
+        {
+            if (Test2.Visibility == Visibility.Visible)
+            {
+                Test2.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Test2.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
